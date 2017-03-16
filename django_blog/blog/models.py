01669b0314b8ca3d6cfcf24b4a04bd34e12e70ff
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-03-16 14:50:25 -0500

    Added more test blog entries

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
