ee165fef5f3f6e152a0dce1bc91a866ccae3dc26
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-07-18 11:39:29 -0500

    Added commenting functionality to blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 from django.views.generic import ListView
 
 
@@ -24,7 +25,24 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to db yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the db
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html',
+                  {'post': post, 'comments': comments,
+                   'comment_form': comment_form})
 
 
 class PostListView(ListView):
