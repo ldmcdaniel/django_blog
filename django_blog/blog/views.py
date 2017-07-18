d788b467268d37cbb9314b0b02768cb48ed9c15a
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-07-18 16:10:24 -0500

    Added Similar Posts to each blog to finish Chp.2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from .models import Post, Comment
 from .forms import CommentForm
 from django.views.generic import ListView
@@ -48,9 +49,18 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
                   {'post': post, 'comments': comments,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
